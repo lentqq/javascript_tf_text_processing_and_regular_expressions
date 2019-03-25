37c7f7981fd085aeb37a69de783ea6a7f728c931
lentqq <lentqq@gmail.com>
2019-03-25 06:58:58 +0200

    Add new file

---
+++ 05.count_string_occurrences.js
@@ -0,0 +1,13 @@
+function countStringsOccurrences(string, searshedWord) {
+    let matchWord = string.split(' ');
+    let count = 0;
+
+    for (const word of matchWord) {
+        if (word === searshedWord) {
+            count++;
+        }
+    }
+     console.log(count);
+}
+
+countStringsOccurrences('This is a word and it also is sentence', 'is')
